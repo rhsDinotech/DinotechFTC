6e8e087830d263721660c810e01f874b497559dc
Ethan <ethan@binarycaer.com>
2018-12-14 13:43:56 -0500

    fixed errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dinotech_Auto_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dinotech_Auto_Far.java
@@ -78,7 +78,7 @@ public class Dinotech_Auto_Far extends LinearOpMode {
     static final double     DRIVE_GEAR_REDUCTION    = .25 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 2.5 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.5;
 
